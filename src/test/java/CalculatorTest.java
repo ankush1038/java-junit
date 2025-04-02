213428116cab122e3607ca31ebca4f9da68bd06a
ankush1038 <ankusharma1029@gmail.com>
2025-04-02 15:01:39 +0530

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(5, calc.add(2, 3));
+        assertEquals(-1, calc.add(-2, 1));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(2, calc.subtract(5, 3));
+        assertEquals(-5, calc.subtract(-2, 3));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(10, calc.multiply(2, 5));
+        assertEquals(-6, calc.multiply(-2, 3));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calc.divide(6, 3));
+        assertEquals(-3, calc.divide(-9, 3));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            calc.divide(5, 0);
+        });
+        assertEquals("Cannot divide by zero", exception.getMessage());
+    }
+}
