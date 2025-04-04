ecf993fb9b28b02a3a0403ee3faca38a86ff16e9
ankush1038 <ankusharma1029@gmail.com>
2025-04-04 14:09:50 +0530

    [Ankush] Add: Test Cases created for even numbers

--- src/test/java/com/bridgelabz/EvenNumbersUtil.java
+++ src/test/java/com/bridgelabz/EvenNumbersUtil.java
@@ -2,23 +2,36 @@ package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class EvenNumbersUtil {
 
-    @Test
-    void shouldCheckEvenNumber(){
-        Assertions.assertTrue(EvenNumbers.isEven(2));
-        Assertions.assertTrue(EvenNumbers.isEven(4));
-        Assertions.assertTrue(EvenNumbers.isEven(6));
-        Assertions.assertTrue(EvenNumbers.isEven(8));
+
+//    void shouldCheckEvenNumber(){
+//        Assertions.assertTrue(EvenNumbers.isEven(2));
+//        Assertions.assertTrue(EvenNumbers.isEven(4));
+//        Assertions.assertTrue(EvenNumbers.isEven(6));
+//        Assertions.assertTrue(EvenNumbers.isEven(8));
+//    }
+//
+//    @Test
+//    void shouldCheckOddNumber(){
+//        Assertions.assertFalse(EvenNumbers.isEven(7));
+//        Assertions.assertFalse(EvenNumbers.isEven(9));
+//    }
+
+
+    @ValueSource( ints = {2,4,6,8})
+
+    void shouldCheckEvenNumber(int number){
+        Assertions.assertTrue(EvenNumbers.isEven(number));
     }
 
-    @Test
-    void shouldCheckOddNumber(){
-        Assertions.assertFalse(EvenNumbers.isEven(7));
-        Assertions.assertFalse(EvenNumbers.isEven(9));
+    @ParameterizedTest
+    @ValueSource( ints = {7,9})
+    void shouldCheckOddNumber(int number){
+        Assertions.assertFalse(EvenNumbers.isEven(number));
     }
-
-    // @ParameterizedTest
 
 }
